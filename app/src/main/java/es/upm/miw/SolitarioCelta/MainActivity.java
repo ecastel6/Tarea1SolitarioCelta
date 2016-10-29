cd66abf6078c2325558a87c931895c59338bb219
Emilio Castellano <ecastel6@gmail.com>
2016-10-29 19:32:03 +0200

    Inicio del repositorio

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
@@ -0,0 +1,183 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends Activity {
+    final String LOG_TAG="MIW16";
+    static final String JUEGOSALVADO = "celta.db";
+
+    JuegoCelta juego;
+
+    private final String GRID_KEY = "GRID_KEY";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        juego = new JuegoCelta();
+        mostrarTablero();
+    }
+
+    /**
+     * Se ejecuta al pulsar una ficha
+     * Las coordenadas (i, j) se obtienen a partir del nombre, ya que el botón
+     * tiene un identificador en formato pXY, donde X es la fila e Y la columna
+     *
+     * @param v Vista de la ficha pulsada
+     */
+    public void fichaPulsada(View v) {
+        String resourceName = getResources().getResourceEntryName(v.getId());
+        int i = resourceName.charAt(1) - '0';   // fila
+        int j = resourceName.charAt(2) - '0';   // columna
+
+        juego.jugar(i, j);
+
+        mostrarTablero();
+        if (juego.juegoTerminado()) {
+            // TODO guardar puntuación
+            new AlertDialogFragment().show(getFragmentManager(), "ALERT DIALOG");
+        }
+    }
+
+    /**
+     * Visualiza el tablero
+     */
+    public void mostrarTablero() {
+        RadioButton button;
+        String strRId;
+        String prefijoIdentificador = getPackageName() + ":id/p"; // formato: package:type/entry
+        int idBoton;
+
+        for (int i = 0; i < JuegoCelta.TAMANIO; i++)
+            for (int j = 0; j < JuegoCelta.TAMANIO; j++) {
+                strRId = prefijoIdentificador + Integer.toString(i) + Integer.toString(j);
+                idBoton = getResources().getIdentifier(strRId, null, null);
+                if (idBoton != 0) { // existe el recurso identificador del botón
+                    button = (RadioButton) findViewById(idBoton);
+                    button.setChecked(juego.obtenerFicha(i, j) == JuegoCelta.FICHA);
+                }
+            }
+    }
+
+    /**
+     * Guarda el estado del tablero (serializado)
+     *
+     * @param outState Bundle para almacenar el estado del juego
+     */
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(GRID_KEY, juego.serializaTablero());
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Recupera el estado del juego
+     *
+     * @param savedInstanceState Bundle con el estado del juego almacenado
+     */
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String grid = savedInstanceState.getString(GRID_KEY);
+        juego.deserializaTablero(grid);
+        mostrarTablero();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.opciones_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcAjustes:
+                startActivity(new Intent(this, SCeltaPrefs.class));
+                return true;
+            case R.id.opcAcercaDe:
+                startActivity(new Intent(this, AcercaDe.class));
+                return true;
+            case R.id.opcReiniciarPartida:
+                juego.reiniciar();
+                mostrarTablero();
+                return true;
+            case R.id.opcGuardarPartida:
+                String partidaSalvada = juego.serializaTablero();
+                //File file = new File(this.getApplicationContext().getFilesDir(),JUEGOSALVADO);
+                FileOutputStream outputStream;
+                try{
+                    outputStream=openFileOutput(JUEGOSALVADO,getApplicationContext().MODE_PRIVATE);
+                    //outputStream.write(partidaSalvada,);
+                    outputStream.close();
+                    Log.i(LOG_TAG,"Generado archivo " + JUEGOSALVADO);
+                } catch (Exception e){
+                    e.printStackTrace();
+                };
+                return true;
+                // TODO!!! resto opciones
+
+            default:
+                Toast.makeText(
+                        this,
+                        getString(R.string.txtSinImplementar),
+                        Toast.LENGTH_SHORT
+                ).show();
+        }
+        return true;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
